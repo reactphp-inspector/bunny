9ddea87ff29f0b1c5f32069bcf203bf101679602
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-12-29 12:13:44 +0100

    Refactored to own vendor

--- src/bunny.php
+++ src/bunny.php
@@ -2,18 +2,18 @@
 
 namespace Bunny;
 
-use WyriHaximus\React\Inspector\GlobalState;
+use ReactInspector\Stream\Bridge;
 
 function fread($handle, $length)
 {
     $data = \fread($handle, $length);
-    GlobalState::incr('eventloop.io.read', (float)strlen($data));
+    Bridge::incr('read', (float)strlen($data));
     return $data;
 }
 
 function fwrite($handle, $data)
 {
     $writtenLength = \fwrite($handle, $data);
-    GlobalState::incr('eventloop.io.write', (float)$writtenLength);
+    Bridge::incr('write', (float)$writtenLength);
     return $writtenLength;
 }
